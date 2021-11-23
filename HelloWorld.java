1855ce301bf8d0e2db7b0e72e897c4cbbed858bf
PunithMohan <87635208+PunithMohan@users.noreply.github.com>
2021-11-24 00:54:50 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ class HelloWorld {
     public static void main(String[] args) {
         
         //added line for label tset
-        // add new line
+       
         //added new line for lasbel test
         System.out.println("Hello, World!"); 
     }
