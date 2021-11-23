2e6e1c817714cb044c425c356f68f16474672bd3
PunithMohan <87635208+PunithMohan@users.noreply.github.com>
2021-11-24 00:45:23 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-  // lone added for testinh
         //added another line for tag test
         // add new line
         System.out.println("Hello, World!"); 
